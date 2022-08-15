73e4db89c316096dd58c1a914d07145cd6c4021c
DimaXDD <dimatruba2004@yandex.ru>
2022-08-15 18:07:13 +0300

    Add Lab1

---
+++ Lab1/Lab1_lin.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+//0 1 1 2 3 5 8 13 21
+int Fibonachi_Cikl(int N) {
+	int mass[3] = { 0, 1 };
+	N = N - 2;
+	for (int c = 0; c <= N; c++) {
+		mass[2] = mass[1];
+		mass[1] = mass[1] + mass[0];
+		mass[0] = mass[2];
+	}
+	return mass[0];
+}
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_ALL, "RUS");
+	double N, time1, time2, time_razn;
+	cout << "Введите N-ое число ряда Фибоначчи: ";
+	cin >> N;
+	int a = 1, b = 1, c;
+	unsigned int start_time = clock();
+	cout << "Число = " << Fibonachi_Cikl(N) << endl;
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	cout << "\nВремя прохождения цикла: " << search_time << " мс." << endl;
+}
---
+++ Lab1/Lab1_recur.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+int number_fibonachi(int counter)
+{
+	if (counter == 0)
+	{
+		return 0;
+	}
+	if (counter == 1)
+	{
+		return 1;
+	}
+	return number_fibonachi(counter - 1) + number_fibonachi(counter - 2);
+}
+
+int main()
+{
+	int rec;
+	setlocale(LC_ALL, "RUS");
+	int N;
+	cout << "Введите число: ";
+	cin >> N;
+	unsigned int start_time = clock();
+	rec = number_fibonachi(N - 1);
+	unsigned int end_time = clock();
+	unsigned int search_time = end_time - start_time;
+	cout << rec << endl;
+	cout << "Время выполнения рекурсии: " << search_time << " мс." << endl;
+	return 0;
+
+}
